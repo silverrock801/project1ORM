db8b134f4fde5e6367730d9daf57f8868deca007
silverrock801 <jamesp801@gmail.com>
2022-01-02 19:36:54 -0500

    added ignore annotation and shells for connection and sql queries

---
+++ Project1ORM/src/main/java/com/revature/persistence/Queries.java
@@ -0,0 +1,5 @@
+package com.revature.persistence;
+
+public class Queries {
+
+}
---
+++ Project1ORM/src/main/java/com/revature/util/ConnectionUtil.java
@@ -0,0 +1,10 @@
+package com.revature.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class ConnectionUtil {
+
+}
